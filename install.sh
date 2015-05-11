3a74d1167a2484d36bf6b49767e0e8f48a5ec97b
Joakim Gustin <jocke.gustin@gmail.com>
2015-05-11 11:53:00 +0200

    Install vundle with install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 # Symlink all files and folders
 ln -s $(pwd)/.vimrc ${HOME}/.vimrc
 ln -s $(pwd)/.vim ${HOME}/.vim
+
+# Install Vundle (for installing the rest of the vim plugins)
+git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
