6dccebb4d0860c6646c7a72d22551dc3659a6740
Joakim Gustin <jocke.gustin@gmail.com>
2015-05-15 15:22:13 +0200

    Also install tmux.conf

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -s $(pwd)/.vimrc ${HOME}/.vimrc
 ln -s $(pwd)/.vim ${HOME}/.vim
 ln -s $(pwd)/.agignore ${HOME}/.agignore
+ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
 
 # Install Vundle (for installing the rest of the vim plugins)
 git clone https://github.com/gmarik/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
