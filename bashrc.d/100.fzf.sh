ef6fa3dffa3861dab3815ce38adf23e46fe8e7f9
Joakim Gustin <joakim.gustin@einride.tech>
2022-11-30 10:56:08 +0100

    bash: better fzf linux support

--- bashrc.d/100.fzf.sh
+++ bashrc.d/100.fzf.sh
@@ -1,4 +1,9 @@
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+if [ -f ~/.fzf.bash ]; then
+	source ~/.fzf.bash
+else
+	[ -f /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
+	[ -f /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
+fi
 
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
 export FZF_DEFAULT_OPTS='--bind J:down,K:up --reverse '
