6050868624d9e178a7b78ec21e04ad77aaa5d459
Joakim Gustin <joakim.gustin@einride.tech>
2023-11-21 13:48:06 +0100

    fix lazy options key

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -4,7 +4,7 @@ return {
    keys = {
       { "<leader>kb", "<cmd>NvimTreeToggle<cr>", desc = "NvimTreeToggle" },
    },
-   config = {
+   opts = {
       renderer = {
          icons = {
             show = {
