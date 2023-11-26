2991f16e7568357bb40dd94ace82982af4f0179a
Joakim Gustin <jocke.gustin@gmail.com>
2023-11-26 09:11:00 +0100

    nvim: lazy load treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,16 @@ return {
       "nvim-treesitter/nvim-treesitter",
       version = false, -- last release is way too old and doesn't work on Windows
       build = ":TSUpdate",
+      event = { "VeryLazy" },
+      init = function(plugin)
+         -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+         -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+         -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+         -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+         -- during startup.
+         require("lazy.core.loader").add_to_rtp(plugin)
+         require("nvim-treesitter.query_predicates")
+      end,
       dependencies = {
          {
             "nvim-treesitter/nvim-treesitter-textobjects",
@@ -47,6 +57,10 @@ return {
             "bash",
             "c",
             "diff",
+            "go",
+            "gomod",
+            "gowork",
+            "gosum",
             "html",
             "javascript",
             "jsdoc",
