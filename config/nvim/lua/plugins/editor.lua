8cbd5d9980c4bb888eb63a664f28ea3bca485851
Joakim Gustin <joakim.gustin@einride.tech>
2023-12-01 08:06:45 +0100

    nvim: try to be lighter on gopls

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -80,7 +80,7 @@ return {
       opts = {
          formatters_by_ft = {
             lua = { "stylua" },
-            go = { "goimports" },
+            go = { "goimports", "gofumpt" },
          },
          format_on_save = {
             timeout_ms = 500,
