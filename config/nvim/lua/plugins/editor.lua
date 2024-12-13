0c306d547ed7eaab11cede0eefa752307951667d
Joakim Gustin <jocke.gustin@gmail.com>
2024-12-13 22:06:27 +0100

    nvim: use lua based tmux navigator

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -11,7 +11,18 @@ return {
       end,
    },
    { -- navigate tmux and vim splits
-      "christoomey/vim-tmux-navigator",
+      "alexghergh/nvim-tmux-navigation",
+      config = function()
+         require("nvim-tmux-navigation").setup({
+            disable_when_zoomed = true, -- defaults to false
+            keybindings = {
+               left = "<C-h>",
+               down = "<C-j>",
+               up = "<C-k>",
+               right = "<C-l>",
+            },
+         })
+      end,
    },
    { -- fzf based navigation and search
       "ibhagwan/fzf-lua",
