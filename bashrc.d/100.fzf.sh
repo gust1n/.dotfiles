3ae1133c4e660777436e306e2047cc0cbdbc125c
gust1n <joakim.gustin@einride.tech>
2025-03-28 10:39:41 +0100

    fix proper fzf bash integration

--- bashrc.d/100.fzf.sh
+++ bashrc.d/100.fzf.sh
@@ -1,8 +1,5 @@
-if [ -f ~/.fzf.bash ]; then
-	source ~/.fzf.bash
-else
-	[ -f /usr/share/fzf/key-bindings.bash ] && source /usr/share/fzf/key-bindings.bash
-	[ -f /usr/share/fzf/completion.bash ] && source /usr/share/fzf/completion.bash
+if command -v go >/dev/null; then
+	eval "$(fzf --bash)"
 fi
 
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
