81449f73deef290c3d84fe524f96cf1d6b23dee7
Joakim Gustin <jocke.gustin@gmail.com>
2025-07-01 10:53:41 +0200

    nvim: restore cursor pos when open buffer

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -21,3 +21,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
+
+-- Restore last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Restore cursor position when opening file",
+  group = vim.api.nvim_create_augroup("RestoreCursor", { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
