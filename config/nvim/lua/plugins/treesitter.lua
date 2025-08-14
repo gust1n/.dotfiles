0d8d01bf5707e6a6e303136c5c946fcf28decc80
Joakim Gustin <jocke.gustin@gmail.com>
2025-08-14 07:33:40 +0200

    nvim: go treesitter injections

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -7,41 +7,40 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    opts = {
-      -- Core features you actually use
-      highlight = { enable = true },
-      indent = { enable = true },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        fold = { enable = true },
 
-      -- Automatic parser installation - installs parsers as you open files
-      auto_install = true,
+        -- Automatic parser installation - installs parsers as you open files
+        auto_install = true,
 
-      -- Treesitter-based folding
-      fold = { enable = true },
-
-      -- Incremental selection (mapped to Ctrl-Space in keymaps)
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-
-      -- Basic text objects for functions and classes
-      textobjects = {
-        select = {
+        -- Incremental selection (mapped to Ctrl-Space in keymaps)
+        incremental_selection = {
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj
           keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
           },
         },
-      },
-    },
+
+        -- Basic text objects for functions and classes
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+    end,
   },
 }
