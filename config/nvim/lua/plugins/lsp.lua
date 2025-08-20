9a81e91256f5cc7f46e15880ab02f37898a7ceac
gust1n <joakim.gustin@cerve.com>
2025-08-20 09:13:39 +0200

    nvim: remove blink

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,6 @@ return {
 
       -- Basic capabilities
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
       capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = { "documentation", "detail", "additionalTextEdits" },
       }
