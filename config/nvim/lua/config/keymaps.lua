a7eba97338b97819c33ea9631f9b74731f5e573a
gust1n <joakim.gustin@cerve.com>
2025-08-24 23:27:46 +0200

    nvim: signature help in insert mode

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -56,6 +56,9 @@ function M.setup_lsp_mappings(bufnr)
   vim.keymap.set("n", "K", function()
     vim.lsp.buf.hover()
   end, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
+  vim.keymap.set("i", "<C-k>", function()
+    vim.lsp.buf.signature_help()
+  end, vim.tbl_extend("force", opts, { desc = "Signature help" }))
 
   -- Code actions and refactoring
   vim.keymap.set("n", "<leader>lr", function()
