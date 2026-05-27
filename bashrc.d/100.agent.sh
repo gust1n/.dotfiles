851d842d26fdb86be6d942256820bb357a085010
Joakim Gustin <jocke.gustin@gmail.com>
2026-05-27 11:56:16 +0200

    agent: switch from tmux windows to sessions with custom top bar

--- bashrc.d/100.agent.sh
+++ bashrc.d/100.agent.sh
@@ -4,6 +4,6 @@ PATH+=":$HOME/Code/dotfiles/bin"
 # Completion for agent subcommands
 _agent_completions() {
   local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "new ls switch kill send capture jump-next help" -- "$cur") )
+  COMPREPLY=( $(compgen -W "new ls switch kill send capture tidy jump-next help" -- "$cur") )
 }
 complete -F _agent_completions agent
