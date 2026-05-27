67e108d0c789ee2f86ac7935074487baf7259605
Joakim Gustin <jocke.gustin@gmail.com>
2026-05-27 11:56:16 +0200

    term: source ghostty shell integration

--- bashrc.d/100.term.sh
+++ bashrc.d/100.term.sh
@@ -1,1 +1,6 @@
 export TERM=screen-256color
+
+# Source Ghostty shell integration (defines __ghostty_hook used in PROMPT_COMMAND)
+if [[ -n "${GHOSTTY_RESOURCES_DIR:-}" ]]; then
+  source "${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash" 2>/dev/null || true
+fi
